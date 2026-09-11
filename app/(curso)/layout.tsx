a039ab6e663a364ef30ffd193af4a392f47b98ca
Freider Achic <freider8998@gmail.com>
2026-09-10 19:06:41 -0500

    fix(curso): corregir LayoutProps en layout de (curso)

--- app/(curso)/layout.tsx
+++ app/(curso)/layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { TallerSidebar } from "@/components/taller/taller-sidebar";
 
-export default function TallerLayout({ children }: LayoutProps<"/taller">) {
+export default function TallerLayout({ children }: LayoutProps<"/">) {
   return (
     <SidebarProvider>
       <TallerSidebar />
